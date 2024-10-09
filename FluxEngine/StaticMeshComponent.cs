f7fd54442a29a087cf177d3a7d01dd9f0b1592d4
Mathias Lefebre <54477532+RadioArtz@users.noreply.github.com>
2024-10-09 20:51:00 +0200

    got rid of StaticMeshAsset var in its component

--- FluxEngine/StaticMeshComponent.cs
+++ FluxEngine/StaticMeshComponent.cs
@@ -5,20 +5,20 @@ namespace Flux.Types
 {
     public class StaticMeshComponent : BaseComponent
     {
-        public StaticMeshAsset _staticMesh;
-
         public Material _material;
 
         private int VertexBufferObject;
         private int VertexArrayObject;
         private int ElementBufferObject;
 
+        private int indicesCount;
+
         public StaticMeshComponent(StaticMeshAsset staticMesh, Material mat)
         {
-            _staticMesh = staticMesh;
             _material = mat;
+            indicesCount = staticMesh.Indices.Length;
 
-            float[] mergedMeshData = MergedMeshArray();
+            float[] mergedMeshData = MergedMeshArray(staticMesh.Vertices,staticMesh.UVCoords,staticMesh.Normals);
 
             VertexBufferObject = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
@@ -41,7 +41,7 @@ namespace Flux.Types
             
             ElementBufferObject = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, _staticMesh.Indices.Length * sizeof(uint), _staticMesh.Indices, BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indicesCount * sizeof(uint), staticMesh.Indices, BufferUsageHint.StaticDraw);
 
             _material._shader.Use();
 
@@ -51,26 +51,26 @@ namespace Flux.Types
         {
             GL.BindVertexArray(VertexArrayObject);
             _material.Render(ParentObject.TransformComponent);
-            GL.DrawElements(PrimitiveType.Triangles, _staticMesh.Indices.Length, DrawElementsType.UnsignedInt, 0);
+            GL.DrawElements(PrimitiveType.Triangles, indicesCount, DrawElementsType.UnsignedInt, 0);
         }
 
-        private float[] MergedMeshArray()
+        private float[] MergedMeshArray(float[] verts, float[] uvs, float[] normals)
         {
             var tmplist = new List<float>();
-            int vertexCount = _staticMesh.Vertices.Length / 3;
+            int vertexCount = verts.Length / 3;
 
             for (int i = 0; i < vertexCount; i++)
             {
-                tmplist.Add(_staticMesh.Vertices[i * 3]);
-                tmplist.Add(_staticMesh.Vertices[i * 3 + 1]);
-                tmplist.Add(_staticMesh.Vertices[i * 3 + 2]);
+                tmplist.Add(verts[i * 3]);
+                tmplist.Add(verts[i * 3 + 1]);
+                tmplist.Add(verts[i * 3 + 2]);
 
-                tmplist.Add(_staticMesh.UVCoords[i * 2]);
-                tmplist.Add(_staticMesh.UVCoords[i * 2 + 1]);
+                tmplist.Add(uvs[i * 2]);
+                tmplist.Add(uvs[i * 2 + 1]);
 
-                tmplist.Add(_staticMesh.Normals[i * 3]);
-                tmplist.Add(_staticMesh.Normals[i * 3 + 1]);
-                tmplist.Add(_staticMesh.Normals[i * 3 + 2]);
+                tmplist.Add(normals[i * 3]);
+                tmplist.Add(normals[i * 3 + 1]);
+                tmplist.Add(normals[i * 3 + 2]);
             }
             return tmplist.ToArray();
         }
--- FluxGame/TestScene.cs
+++ FluxGame/TestScene.cs
@@ -11,7 +11,9 @@ namespace FluxGame
     {
         AActor RenderTesterActor;
         MeshRef myAwesomeMesh;
+        MeshRef myAwesomeMesh2;
         AActor myAwesomeCamera;
+
         public override void OnLoad()
         {
             base.OnLoad();
@@ -21,8 +23,10 @@ namespace FluxGame
             myAwesomeCamera.AddComponent(new EditorCamera(Engine.window));
             Debug.Log("Loading Mesh!");
             myAwesomeMesh = MeshLoader.LoadMeshFromFile(@"A:\_Terrain.obj");
+            myAwesomeMesh2 = MeshLoader.LoadMeshFromFile(@"A:\_Terrain2.obj");
             RenderTesterActor = new QuadActor();
             RenderTesterActor.AddComponent(new StaticMeshComponent(MeshLoader.GetMeshAssetFromRef(myAwesomeMesh),new WhiteMat()));
+            RenderTesterActor.AddComponent(new StaticMeshComponent(MeshLoader.GetMeshAssetFromRef(myAwesomeMesh2), new WhiteMat()));
         }
     }
 }
