b4d117ce0654e0048f64d5ac15f69e4bdba41e65
Mathias Lefebre <54477532+RadioArtz@users.noreply.github.com>
2025-01-06 17:09:41 +0100

    Add ExampleScene

---
+++ FluxGame/ExampleScene.cs
@@ -0,0 +1,17 @@
+﻿using Flux.Types;
+
+namespace FluxGame
+{
+    public class ExampleScene : FScene
+    {
+        public override void OnLoad()
+        {
+            base.OnLoad();
+        }
+
+        public override void OnTick(float delta)
+        {
+            base.OnTick(delta);
+        }
+    }
+}
